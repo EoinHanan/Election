692f4dcfc16fa07df211959a496a408795a92171
Eoin Hanan <14176459@studentmail.ul.ie>
2017-06-15 14:21:05 +0200

    Commit it all!

---
+++ src/PercentageCalculator.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+/**
+ * Created by EoinH on 10/06/2017.
+ */
+public class PercentageCalculator extends Calculator{
+    ArrayList<Candidate> candidates;
+    ArrayList <Constituency> constituencies;
+    ArrayList <Party> parties;
+    ArrayList <Round> rounds;
+    PercentageCalculator (ArrayList<Candidate> candidates, ArrayList<Constituency> constituencies, ArrayList<Party> parties, ArrayList<Round> rounds){
+        this.candidates = candidates;
+        this.constituencies = constituencies;
+        this.parties = parties;
+        this.rounds = rounds;
+    }
+
+    public void CalculateConstituency(){
+
+    }
+
+    public void CalculateCandidates(){
+
+    }
+
+    public void CalculateParties(){
+        for (int i =0; i < candidates.size();i++){
+
+        }
+    }
+
+
+}
